dbf7c4ca5eeaca20196b872b9af9b8518f7c2372
Josh Stein <josh.e.stein@gmail.com>
2026-03-16 00:02:13 +0200

    Input for user name during registration

--- lib/niceties_web/live/user_live/registration.ex
+++ lib/niceties_web/live/user_live/registration.ex
@@ -24,13 +24,20 @@ defmodule NicetiesWeb.UserLive.Registration do
 
         <.form for={@form} id="registration_form" phx-submit="save" phx-change="validate">
           <.input
+            field={@form[:name]}
+            type="text"
+            label="Name"
+            autocomplete="name"
+            required
+            phx-mounted={JS.focus()}
+          />
+          <.input
             field={@form[:email]}
             type="email"
             label="Email"
             autocomplete="username"
             spellcheck="false"
             required
-            phx-mounted={JS.focus()}
           />
 
           <.button phx-disable-with="Creating account..." class="btn btn-primary w-full">
@@ -49,7 +56,7 @@ defmodule NicetiesWeb.UserLive.Registration do
   end
 
   def mount(_params, _session, socket) do
-    changeset = Accounts.change_user_email(%User{}, %{}, validate_unique: false)
+    changeset = Accounts.change_user(%User{})
 
     {:ok, assign_form(socket, changeset), temporary_assigns: [form: nil]}
   end
@@ -78,7 +85,7 @@ defmodule NicetiesWeb.UserLive.Registration do
   end
 
   def handle_event("validate", %{"user" => user_params}, socket) do
-    changeset = Accounts.change_user_email(%User{}, user_params, validate_unique: false)
+    changeset = Accounts.change_user(%User{}, user_params)
     {:noreply, assign_form(socket, Map.put(changeset, :action, :validate))}
   end
 

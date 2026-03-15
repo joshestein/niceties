69fbf8919a47b8babfc6ac276ceb7eac627456a9
Josh Stein <josh.e.stein@gmail.com>
2026-03-15 22:50:10 +0200

    Use `citext` for email

--- priv/repo/migrations/20260315124718_create_users.exs
+++ priv/repo/migrations/20260315124718_create_users.exs
@@ -2,8 +2,10 @@ defmodule Niceties.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
+
     create table(:users) do
-      add :email, :string
+      add :email, :citext, null: false
       add :name, :string
 
       timestamps(type: :utc_datetime)

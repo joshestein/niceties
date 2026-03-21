4130f907e66605fc7eada81993affed87f995b28
Josh Stein <josh.e.stein@gmail.com>
2026-03-20 21:08:54 -0700

    Build maps of given -> nicety and received -> nicety

--- lib/niceties_web/controllers/group_controller.ex
+++ lib/niceties_web/controllers/group_controller.ex
@@ -1,14 +1,29 @@
 defmodule NicetiesWeb.GroupController do
   use NicetiesWeb, :controller
 
+  alias Niceties.Groups
+  alias Niceties.Notes
+
   def home(conn, _params) do
     render(conn, :home)
   end
 
   def group(conn, %{"id" => id}) do
-    render(conn, :group, id: id)
-  end
+    # TODO: check user belongs to group. If not, redirect to /groups
+    received = Notes.get_received(conn.assigns.current_scope, id)
+    received_map = Map.new(received, fn nicety -> {nicety.user_from_id, nicety} end)
+    given = Notes.get_given(conn.assigns.current_scope, id)
+    given_map = Map.new(given, fn nicety -> {nicety.user_to_id, nicety} end)
+    members = Groups.get_all_members(id)
 
-  def nicety(conn, _params) do
+    other_users =
+      Enum.filter(members, fn member -> member.id != conn.assigns.current_scope.user.id end)
+
+    render(conn, :group,
+      id: id,
+      received_map: received_map,
+      given_map: given_map,
+      users: other_users
+    )
   end
 end

cb5006b09823b3f1c7d28648d2dea42570969a57
Josh Stein <josh.e.stein@gmail.com>
2026-03-20 21:30:50 -0700

    Seed some data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Niceties.Repo.delete_all(Niceties.Notes.Nicety)
+Niceties.Repo.delete_all(Niceties.Groups.Membership)
+Niceties.Repo.delete_all(Niceties.Groups.Group)
+Niceties.Repo.delete_all(Niceties.Accounts.User)
+
+{:ok, user} = Niceties.Accounts.create_user(%{email: "mycoolemail@gmail.com", name: "John Doe"})
+{:ok, group} = Niceties.Groups.create_group(%{name: "my_group", releases_at: ~U[2026-06-01 18:00:00Z]})
+Niceties.Groups.create_membership(%{role: "admin", group_id: group.id, user_id: user.id})
+
+{:ok, fellow} = Niceties.Accounts.create_user(%{email: "fellow@gmail.com", name: "Fellow 1"})
+{:ok, fellow_2} = Niceties.Accounts.create_user(%{email: "fellow2@gmail.com", name: "Fellow 2"})
+
+Niceties.Groups.create_membership(%{role: "fellow", group_id: group.id, user_id: fellow.id})
+Niceties.Groups.create_membership(%{role: "fellow", group_id: group.id, user_id: fellow_2.id})

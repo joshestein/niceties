67a3fa4b6e9b670b70c40d32e18e83473a5a83fe
Josh Stein <josh.e.stein@gmail.com>
2026-03-24 14:48:38 -0700

    Unordered list

--- lib/niceties_web/controllers/admin_html.ex
+++ lib/niceties_web/controllers/admin_html.ex
@@ -4,11 +4,13 @@ defmodule NicetiesWeb.AdminHTML do
   def groups(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
-      <li :for={group <- @groups} id={"group-#{group.id}"}>
-        <.link href={~p"/admin/groups/#{group.id}"} class="hover:underline">
-          {group.name} - {group.id}
-        </.link>
-      </li>
+      <ul>
+        <li :for={group <- @groups} id={"group-#{group.id}"}>
+          <.link href={~p"/admin/groups/#{group.id}"} class="hover:underline">
+            {group.name} - {group.id}
+          </.link>
+        </li>
+      </ul>
     </Layouts.app>
     """
   end

1c83febea4824da0ea4f7daff2f39729cbfeaede
Josh Stein <josh.e.stein@gmail.com>
2026-03-24 14:02:08 -0700

    Initial `group` UI for admin

--- lib/niceties_web/controllers/admin_controller.ex
+++ lib/niceties_web/controllers/admin_controller.ex
@@ -12,8 +12,10 @@ defmodule NicetiesWeb.AdminController do
 
   end
 
-  def group(conn, _params) do
-
+  def group(conn, %{"id" => id}) do
+    group = Groups.get_group!(id)
+    users = Groups.get_all_users(id)
+    render(conn, users: users, group: group)
   end
 
   def release(conn, _params) do
--- lib/niceties_web/controllers/admin_html.ex
+++ lib/niceties_web/controllers/admin_html.ex
@@ -12,4 +12,19 @@ defmodule NicetiesWeb.AdminHTML do
     </Layouts.app>
     """
   end
+
+  def group(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash}>
+      <.link href={~p"/admin/groups"} class="hover:underline">← Back to all groups</.link>
+
+      <h1>Group: {@group.name}</h1>
+
+      <.table id="users" rows={@users}>
+        <:col :let={user} label="Name">{user.name}</:col>
+        <:col :let={user} label="Email">{user.email}</:col>
+      </.table>
+    </Layouts.app>
+    """
+  end
 end

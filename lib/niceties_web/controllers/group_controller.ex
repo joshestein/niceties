47d9dde19ca4572d55972aa0a19ce85caf31c034
Josh Stein <josh.e.stein@gmail.com>
2026-03-26 23:58:15 -0400

    Woops, `before` not `after`

--- lib/niceties_web/controllers/group_controller.ex
+++ lib/niceties_web/controllers/group_controller.ex
@@ -53,7 +53,7 @@ defmodule NicetiesWeb.GroupController do
         is_nil(group.releases_at) ->
           nil
 
-        DateTime.after?(group.releases_at, DateTime.utc_now()) ->
+        DateTime.before?(group.releases_at, DateTime.utc_now()) ->
           Notes.get_received(conn.assigns.current_scope, id)
 
         true ->

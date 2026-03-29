0c918ed99523aabfd6ef524c23f3f03883d622a9
Josh Stein <josh.e.stein@gmail.com>
2026-03-28 20:53:00 -0400

    Implement `require_admin_user`

--- lib/niceties_web/user_auth.ex
+++ lib/niceties_web/user_auth.ex
@@ -271,9 +271,50 @@ defmodule NicetiesWeb.UserAuth do
     end
   end
 
+  @doc """
+  Plug for routes that require the user to be an administrator.
+  """
+  def require_admin_user(conn, _opts) do
+    if conn.assigns.current_scope && conn.assigns.current_scope.user do
+      case conn.params["id"] do
+        nil ->
+          if Enum.empty?(Groups.list_admin_groups_for_user(conn.assigns.current_scope)) do
+            deny_admin_access(conn, :unauthorized)
+          else
+            conn
+          end
+
+        id ->
+          if Groups.is_admin_of_group?(conn.assigns.current_scope.user.id, id) do
+            conn
+          else
+            deny_admin_access(conn, :unauthorized)
+          end
+      end
+    else
+      deny_admin_access(conn, :unauthenticated)
+    end
+  end
+
   defp maybe_store_return_to(%{method: "GET"} = conn) do
     put_session(conn, :user_return_to, current_path(conn))
   end
 
   defp maybe_store_return_to(conn), do: conn
+
+  defp deny_admin_access(conn, :unauthorized) do
+    conn
+    |> put_flash(:error, "You must be an administrator to access this page.")
+    |> maybe_store_return_to()
+    |> redirect(to: ~p"/groups")
+    |> halt()
+  end
+
+  defp deny_admin_access(conn, :unauthenticated) do
+    conn
+    |> put_flash(:error, "You must be logged in to view this page.")
+    |> maybe_store_return_to()
+    |> redirect(to: ~p"/users/log-in")
+    |> halt()
+  end
 end

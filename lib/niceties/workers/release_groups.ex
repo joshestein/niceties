ef2e4d98f180358da0c610df9c2252610ec5f5fd
Josh Stein <josh.e.stein@gmail.com>
2026-03-30 22:18:32 -0400

    Proper cont/halt handling for enqueing release email

--- lib/niceties/workers/release_groups.ex
+++ lib/niceties/workers/release_groups.ex
@@ -9,11 +9,12 @@ defmodule Niceties.Workers.ReleaseGroups do
     Enum.reduce_while(groups, :ok, fn group, _acc ->
       case Groups.update_group(group, %{released: true}) do
         {:ok, group} ->
-          %{group_id: group.id}
-          |> Niceties.Workers.SendReleaseEmails.new()
-          |> Oban.insert()
-
-          {:cont, :ok}
+          case %{group_id: group.id}
+               |> Niceties.Workers.SendReleaseEmails.new()
+               |> Oban.insert() do
+            {:ok, _job} -> {:cont, :ok}
+            {:error, reason} -> {:halt, {:error, "Failed to enqueue email job for group #{group.id}: #{inspect(reason)}"}}
+          end
 
         {:error, _changeset} ->
           {:halt, {:error, "Failed to release group #{inspect(group.id)}"}}

c67db427312ae26891a2c11719a571df388ce2af
Josh Stein <josh.e.stein@gmail.com>
2026-04-06 22:28:40 +0200

    Remove old groups index controller and html

--- lib/niceties_web/controllers/group_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule NicetiesWeb.GroupController do
-  use NicetiesWeb, :controller
-  alias Niceties.Groups
-
-  def index(conn, _params) do
-    groups = Groups.list_groups_for_user(conn.assigns.current_scope)
-    render(conn, :index, groups: groups)
-  end
-end

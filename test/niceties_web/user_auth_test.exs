ea4f3cf54128156abc4ab02d0921718c444b23b3
Josh Stein <josh.e.stein@gmail.com>
2026-04-06 23:07:07 +0200

    Tests for `require_admin_user`

--- test/niceties_web/user_auth_test.exs
+++ test/niceties_web/user_auth_test.exs
@@ -351,6 +351,98 @@ defmodule NicetiesWeb.UserAuthTest do
     end
   end
 
+  describe "require_admin_user/2" do
+    setup %{conn: conn} do
+      %{conn: UserAuth.fetch_current_scope_for_user(conn, [])}
+    end
+
+    test "redirects to groups if user is not admin", %{conn: conn, user: user} do
+      _participant_membership = membership_fixture(%{role: "participant", user_id: user.id})
+      conn =
+        %{conn | path_info: ["admin"], params: %{}}
+        |> assign(:current_scope, Scope.for_user(user))
+        |> fetch_flash()
+        |> UserAuth.require_admin_user([])
+
+      assert conn.halted
+
+      assert redirected_to(conn) == ~p"/groups"
+
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) ==
+               "You must be an administrator to access this page."
+    end
+
+    test "does not redirect if user is admin", %{conn: conn, user: user} do
+      _admin_membership = membership_fixture(%{role: "admin", user_id: user.id})
+      conn =
+        %{conn | path_info: ["admin"], params: %{}}
+        |> assign(:current_scope, Scope.for_user(user))
+        |> fetch_flash()
+        |> UserAuth.require_admin_user([])
+
+      refute conn.halted
+      refute conn.status
+    end
+
+    test "does not redirect if user is admin of the specified group", %{conn: conn, user: user} do
+      admin_membership = membership_fixture(%{role: "admin", user_id: user.id})
+
+      conn =
+        %{conn | params: %{"id" => to_string(admin_membership.group_id)}}
+        |> assign(:current_scope, Scope.for_user(user))
+        |> fetch_flash()
+        |> UserAuth.require_admin_user([])
+
+      refute conn.halted
+      refute conn.status
+    end
+
+    test "redirects if user is admin of a different group but not the specified one", %{
+      conn: conn,
+      user: user
+    } do
+      _admin_membership = membership_fixture(%{role: "admin", user_id: user.id})
+      other_group_membership = membership_fixture(%{role: "admin"})
+
+      conn =
+        %{conn | params: %{"id" => to_string(other_group_membership.group_id)}}
+        |> assign(:current_scope, Scope.for_user(user))
+        |> fetch_flash()
+        |> UserAuth.require_admin_user([])
+
+      assert conn.halted
+      assert redirected_to(conn) == ~p"/groups"
+
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) ==
+               "You must be an administrator to access this page."
+    end
+
+    test "stores the return-to path on GET when access is denied", %{conn: conn, user: user} do
+      conn =
+        %{conn | path_info: ["admin", "groups"], query_string: "page=2", method: "GET", params: %{}}
+        |> assign(:current_scope, Scope.for_user(user))
+        |> fetch_flash()
+        |> UserAuth.require_admin_user([])
+
+      assert conn.halted
+      assert get_session(conn, :user_return_to) == "/admin/groups?page=2"
+    end
+
+    test "does not store the return-to path on non-GET when access is denied", %{
+      conn: conn,
+      user: user
+    } do
+      conn =
+        %{conn | path_info: ["admin", "groups"], method: "POST", params: %{}}
+        |> assign(:current_scope, Scope.for_user(user))
+        |> fetch_flash()
+        |> UserAuth.require_admin_user([])
+
+      assert conn.halted
+      refute get_session(conn, :user_return_to)
+    end
+  end
+
   describe "disconnect_sessions/1" do
     test "broadcasts disconnect messages for each token" do
       tokens = [%{token: "token1"}, %{token: "token2"}]

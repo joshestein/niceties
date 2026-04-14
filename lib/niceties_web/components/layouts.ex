d245257bd5147fcf72eb629a4eb4730a8bf0c3f4
Josh Stein <josh.e.stein@gmail.com>
2026-04-14 23:46:09 +0200

    Reduce top padding

--- lib/niceties_web/components/layouts.ex
+++ lib/niceties_web/components/layouts.ex
@@ -61,7 +61,7 @@ defmodule NicetiesWeb.Layouts do
       </div>
     </header>
 
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
+    <main class="px-4 pt-10 pb-16 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl space-y-4">
         {render_slot(@inner_block)}
       </div>

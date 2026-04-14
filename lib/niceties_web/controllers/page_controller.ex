1607a7687b4c4441b633cbf342787c34e73dcdab
Josh Stein <josh.e.stein@gmail.com>
2026-04-14 23:08:03 +0200

    Render groups on homepage

--- lib/niceties_web/controllers/page_controller.ex
+++ lib/niceties_web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule NicetiesWeb.PageController do
   use NicetiesWeb, :controller
 
+  alias Niceties.Groups
+
   def home(conn, _params) do
-    render(conn, :home)
+    groups =
+      case conn.assigns[:current_scope] do
+        nil -> []
+        scope -> Groups.list_groups_for_user(scope)
+      end
+
+    render(conn, :home, groups: groups)
   end
 end

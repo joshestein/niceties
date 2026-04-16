2424589fb069f1de219919ec8c85812f23e0746c
Josh Stein <josh.e.stein@gmail.com>
2026-04-16 12:35:13 +0200

    Import new `ViewHelpers`

--- lib/niceties_web.ex
+++ lib/niceties_web.ex
@@ -86,6 +86,7 @@ defmodule NicetiesWeb do
       import Phoenix.HTML
       # Core UI components
       import NicetiesWeb.CoreComponents
+      import NicetiesWeb.ViewHelpers
 
       # Common modules used in templates
       alias Phoenix.LiveView.JS

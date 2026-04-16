5d14fcb87322acf4bde9c9d33b8b29fe33767897
Josh Stein <josh.e.stein@gmail.com>
2026-04-16 11:34:20 +0200

    Allow avatar uploading

--- lib/niceties_web/live/user_live/settings.ex
+++ lib/niceties_web/live/user_live/settings.ex
@@ -25,6 +25,11 @@ defmodule NicetiesWeb.UserLive.Settings do
         />
         <.button variant="primary" phx-disable-with="Changing...">Change Email</.button>
       </.form>
+
+      <.form for={%{}} id="avatar_form" phx-submit="upload_avatar" phx-change="validate_avatar">
+        <.live_file_input upload={@uploads.avatar} />
+        <.button type="submit" variant="primary">Upload photo</.button>
+      </.form>
     </Layouts.app>
     """
   end
@@ -51,6 +56,11 @@ defmodule NicetiesWeb.UserLive.Settings do
       socket
       |> assign(:current_email, user.email)
       |> assign(:email_form, to_form(email_changeset))
+      |> allow_upload(:avatar,
+        accept: ~w(.jpg .jpeg .png .webp),
+        max_entries: 1,
+        max_file_size: 5_000_000
+      )
 
     {:ok, socket}
   end
@@ -85,6 +95,41 @@ defmodule NicetiesWeb.UserLive.Settings do
 
       changeset ->
         {:noreply, assign(socket, :email_form, to_form(changeset, action: :insert))}
+    end
+  end
+
+  # phx-change needed for file uploading even though event handler does nothing
+  def handle_event("validate_avatar", _params, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("upload_avatar", _params, socket) do
+    user = socket.assigns.current_scope.user
+
+    result =
+      consume_uploaded_entries(socket, :avatar, fn %{path: path}, _entry ->
+        outcome =
+          with {:ok, image} <- Image.open(path),
+               {:ok, resized} <- Image.thumbnail(image, 128, fit: :cover, height: 128),
+               {:ok, binary} <- Image.write(resized, :memory, suffix: ".webp") do
+            {:ok, binary}
+          end
+
+        {:ok, outcome}
+      end)
+
+    case result do
+      [{:ok, binary}] when is_binary(binary) ->
+        case Accounts.update_user_avatar(user, %{avatar: binary}) do
+          {:ok, _user} ->
+            {:noreply, put_flash(socket, :info, "Avatar updated.")}
+
+          {:error, _changeset} ->
+            {:noreply, put_flash(socket, :error, "Failed to save avatar.")}
+        end
+
+      _ ->
+        {:noreply, put_flash(socket, :error, "Upload failed.")}
     end
   end
 end
